e5fbc0b39af355e5aa0346e168b15cc7d845f501
Gabriel Carneiro <gabriel.carneiro97@live.com>
2020-02-04 15:38:57 -0300

    adicionei um DatePicker a agenda

--- src/components/AgendaMain.tsx
+++ src/components/AgendaMain.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Row, Col, Layout } from 'antd';
-import { red } from '@ant-design/colors';
+import {
+  Row, Col, Layout, DatePicker,
+} from 'antd';
 import Backend from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 
+import moment from 'moment';
 import AgendaBoardDropabble from './AgendaBoardDroppable';
 import { consultaDb } from '../services/db.service';
 
@@ -13,29 +15,36 @@ import { carregarConsultas } from '../store/paciente';
 export default function AgendaMain(): JSX.Element {
   const dispatch = useDispatch();
 
+  const [data, setData] = useState<moment.Moment | null>(moment());
+
   useEffect(() => {
-    consultaDb.findByDate(new Date()).then((c) => (dispatch(carregarConsultas(c))));
-  }, []);
+    consultaDb.findByDate(
+      data ? data.toDate() : new Date(),
+    ).then((c) => (dispatch(carregarConsultas(c))));
+  }, [data]);
 
   return (
     <DndProvider backend={Backend}>
-      <Layout style={{}}>
+      <Layout>
+        <Row style={{ marginBottom: 12 }}>
+          <DatePicker format="DD/MM/YYYY" value={data} onChange={setData} />
+        </Row>
         <Row gutter={4} type="flex" justify="space-between">
-          <Col span={6}>
+          <Col span={4}>
+            <AgendaBoardDropabble title="Não Compareceu" boardIndex={4} />
+          </Col>
+          <Col span={5}>
             <AgendaBoardDropabble title="Agendados" boardIndex={0} />
           </Col>
-          <Col span={6}>
+          <Col span={5}>
             <AgendaBoardDropabble title="Sala de Espera" boardIndex={1} />
           </Col>
-          <Col span={6}>
+          <Col span={5}>
             <AgendaBoardDropabble title="Em Atendimento" boardIndex={2} />
           </Col>
-          <Col span={6}>
+          <Col span={5}>
             <AgendaBoardDropabble title="Atendimento Concluído" boardIndex={3} />
           </Col>
-          {/* <Col span={4}>
-            <AgendaBoardDropabble title="Não Compareceu" boardIndex={4} />
-          </Col> */}
         </Row>
       </Layout>
     </DndProvider>
